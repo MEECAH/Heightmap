a5b733327f03ebe4ac9c6ac742ff097fb632c8c2
MEECAH <micahjhaycraft@gmail.com>
2019-06-12 12:33:32 +0300

    Update weightedAverage.java

--- weightedAverage.java
+++ weightedAverage.java
@@ -1,29 +1,35 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
-public class weightedAverage {
+public class WeightedAverageCalculator {
 
-    ArrayList<Integer> heights;
-    ArrayList<Double> distances;
+    ArrayList<Integer> heights = new ArrayList<>();
+    ArrayList<Double> distances = new ArrayList<>();
 
-    public weightedAverage(){
-    }
-
-
-    public int calculateWA(Point<Integer>[] array){
-        double WA = 0;
-
+    public WeightedAverageCalculator(Point<Integer>[] array){
         for (Point<Integer> p: array
         ) {
             heights.add(p.getValue());
             distances.add(p.getDistance());
         }
+    }
 
-        for(int i=0; i<=heights.size(); i++){
-            WA += ((double)heights.get(i))/(distances.get(i));
+    //calculate weighted average with weights assigned based on distance from sample point
+    public int calculateWA(){
+        double returnWA = 0;
+        double weight = 0;
+        double sumOfWeights = 0;
+
+        for(int i=0; i<heights.size(); i++){
+            weight = 1/(distances.get(i));
+            sumOfWeights += 1/distances.get(i);
+            returnWA += (weight)*(heights.get(i));
         }
 
-        return (int)WA;
+        returnWA = returnWA/sumOfWeights;
+
+        return (int)(returnWA+.5);
     }
 
 }
